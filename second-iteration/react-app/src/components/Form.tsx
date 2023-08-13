8bb7f4e668ebbd965c0bde73f0e66b558655500a
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-08-13 09:05:22 +0530

    react-hook-form lib

--- second-iteration/react-app/src/components/Form.tsx
+++ second-iteration/react-app/src/components/Form.tsx
@@ -1,33 +1,33 @@
-import { useRef } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = {
-    name: "",
-    age: 0,
-  };
+  const { register, handleSubmit } = useForm();
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    if (nameRef.current) person.name = nameRef.current.value;
-    if (ageRef.current) person.age = parseInt(ageRef.current.value);
-    console.log(person);
-  };
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          {...register("name")}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          {...register("age")}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
