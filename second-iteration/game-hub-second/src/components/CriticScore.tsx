02059c4411d96f1814ba95ba14ae44599aa46371
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-08-15 20:51:08 +0530

    game-hub-second displaying critic score

---
+++ second-iteration/game-hub-second/src/components/CriticScore.tsx
@@ -0,0 +1,24 @@
+import { Badge } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const color = score > 75 ? "green" : score > 60 ? "yello" : "red";
+  return (
+    <>
+      <Badge
+        colorScheme={color}
+        fontSize={"14px"}
+        paddingX={2}
+        borderRadius={"6px"}
+      >
+        {score}
+      </Badge>
+    </>
+  );
+};
+
+export default CriticScore;
