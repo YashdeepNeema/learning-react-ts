da5ccefb94bd7439320da7cc794d2eea534e9011
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-08-16 09:07:39 +0530

    game-hub-second display genres

--- second-iteration/game-hub-second/src/components/GenreList.tsx
+++ second-iteration/game-hub-second/src/components/GenreList.tsx
@@ -1,14 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data: genres, error, isLoading } = useGenres();
 
   return (
-    <ul>
+    <List>
       {genres.map((g) => (
-        <li key={g.id}>{g.name}</li>
+        <ListItem key={g.id} marginY={"5px"}>
+          <HStack>
+            <Image
+              boxSize={"32px"}
+              borderRadius={10}
+              src={getCroppedImageUrl(g.image_background)}
+            />
+            <Text fontSize="lg">{g.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
