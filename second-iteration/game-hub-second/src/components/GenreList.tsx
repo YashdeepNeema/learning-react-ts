86fac4622fccd6eed89c337c1e0868cab22976dd
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-08-16 09:11:07 +0530

    game-hub-second show a spinner while fetching geners

--- second-iteration/game-hub-second/src/components/GenreList.tsx
+++ second-iteration/game-hub-second/src/components/GenreList.tsx
@@ -1,10 +1,13 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data: genres, error, isLoading } = useGenres();
 
+  if (isLoading) return <Spinner />;
+
+  if (error) return null;
   return (
     <List>
       {genres.map((g) => (
