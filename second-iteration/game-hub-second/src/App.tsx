b100e63788815f90109b21861abf77ee3a889972
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-08-17 14:59:34 +0530

    game-hub-second add a dynamic heading

--- second-iteration/game-hub-second/src/App.tsx
+++ second-iteration/game-hub-second/src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -7,6 +7,7 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -45,6 +46,9 @@ function App() {
       </Show>
 
       <GridItem area={"main"}>
+        <Box marginLeft={10} marginBottom={5}>
+          <GameHeading gameQuery={gameQuery} />
+        </Box>
         <HStack marginLeft={10}>
           <PlatformSelector
             selectedPlatform={gameQuery.platform as Platform}
