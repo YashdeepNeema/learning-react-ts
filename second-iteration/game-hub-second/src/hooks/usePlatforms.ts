6b299d94112825875f9fcbac5f034d6ff193638b
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-08-17 09:53:30 +0530

    game-hub-second filter games by platform

--- second-iteration/game-hub-second/src/hooks/usePlatforms.ts
+++ second-iteration/game-hub-second/src/hooks/usePlatforms.ts
@@ -3,7 +3,7 @@ import useData from "./useData";
 interface Platform {
   id: number;
   name: string;
-  slud: string;
+  slug: string;
 }
 
 const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
