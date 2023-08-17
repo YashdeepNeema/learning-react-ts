40707b89fe4baca1061a2d0a7d9b7901cacde466
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-08-17 11:01:57 +0530

    game-hub-second sort the games

---
+++ second-iteration/game-hub-second/src/components/SortSelector.tsx
@@ -0,0 +1,43 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import React from "react";
+import { BsChevronDown } from "react-icons/bs";
+
+const sortOrderr = [
+  { value: "", label: "Relevance" },
+  { value: "-added", label: "Date added" },
+  { value: "name", label: "Name" },
+  { value: "-released", label: "Release date" },
+  { value: "-metacritic", label: "Popularity" },
+  { value: "-rating", label: "Average Rating" },
+];
+
+interface Props {
+  sortOrder: string;
+  onSelectSortOrder: (sortOrder: string) => void;
+}
+
+const SortSelector = ({ sortOrder, onSelectSortOrder }: Props) => {
+  const currentSortOrder = sortOrderr.find(
+    (order) => order.value === sortOrder
+  );
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Order by: {currentSortOrder?.label || "Relevance"}
+      </MenuButton>
+      <MenuList>
+        {sortOrderr.map((order) => (
+          <MenuItem
+            key={order.value}
+            value={order.value}
+            onClick={() => onSelectSortOrder(order.value)}
+          >
+            {order.label}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default SortSelector;
